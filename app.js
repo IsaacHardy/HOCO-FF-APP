dd109723f33406c265d72c866c46d25ecc1cb906
Isaac Hardy <isaachardyDEV@gmail.com>
2016-08-07 01:52:34 -0400

    yes

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const upload = multer({ dest: path.join(__dirname, 'uploads') });
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-dotenv.load({ path: '/.env' });
+dotenv.config({ path: '.env' });
 
 /**
  * Controllers (route handlers).
