f27a585890aa82ba7c17e4eb3aaa329ddbd5a5ef
Isaac Hardy <isaachardyDEV@gmail.com>
2016-08-30 20:41:04 -0400

    Attempted fix 1

--- config/passport.js
+++ config/passport.js
@@ -64,17 +64,19 @@ passport.use(new FacebookStrategy({
         req.flash('errors', { msg: 'There is already a Facebook account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
         done(err);
       } else {
-        User.findById(req.user.id, (err, user) => {
-          user.facebook = profile.id;
-          user.tokens.push({ kind: 'facebook', accessToken });
-          user.profile.name = user.profile.name || profile.name.givenName + ' ' + profile.name.familyName;
-          user.profile.gender = user.profile.gender || profile._json.gender;
-          user.profile.picture = user.profile.picture || `https://graph.facebook.com/${profile.id}/picture?type=large`;
-          user.save((err) => {
-            req.flash('info', { msg: 'Facebook account has been linked.' });
-            done(err, user);
+        setTimeout(function() {
+          User.findById(req.user.id, (err, user) => {
+            user.facebook = profile.id;
+            user.tokens.push({ kind: 'facebook', accessToken });
+            user.profile.name = user.profile.name || profile.name.givenName + ' ' + profile.name.familyName;
+            user.profile.gender = user.profile.gender || profile._json.gender;
+            user.profile.picture = user.profile.picture || `https://graph.facebook.com/${profile.id}/picture?type=large`;
+            user.save((err) => {
+              req.flash('info', { msg: 'Facebook account has been linked.' });
+              done(err, user);
+            });
           });
-        });
+        },0);
       }
     });
   } else {
